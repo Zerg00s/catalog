0db3ebbf1f936af854d0b15adde2e9795720938b
Denis Molodtsov <denisM@jolera.com>
2016-12-13 11:52:28 -0500

    fixed a bug with email generator

--- src/App/Forms/EmailTemplate/emailApp.js
+++ src/App/Forms/EmailTemplate/emailApp.js
@@ -8,12 +8,10 @@ var controller = app.controller('emailController', function($scope,$sce){
     $scope.body = 'Sample body,\n\nitems:1\nmodel: Osx#124\n\nRegards,\nDenis Molodtsov';
 
 
-
-
     $scope.$watch('[to,cc,subject,body]',function(){
         var mailtto = 'mailto:'+$scope.to +
             '?cc='+$scope.cc +
-            '&subject='+$scope.to +
+            '&subject='+$scope.subject +
             '&body='+$scope.body;
              $scope.link = encodeURI(mailtto);
     }, true);
