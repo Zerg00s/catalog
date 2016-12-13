e40b51d5eb79e32578888380821b8ef01e755ff8
Denis Molodtsov <denisM@jolera.com>
2016-12-13 11:43:57 -0500

    added email template generator

---
+++ src/App/Forms/EmailTemplate/emailApp.js
@@ -0,0 +1,20 @@
+"use strict";
+var app = angular.module('emailApp',[]);
+
+var controller = app.controller('emailController', function($scope,$sce){
+    $scope.to = 'sample@optrustcom; sample2@optrust.com';
+    $scope.cc = 'additiona@optrust.com';
+    $scope.subject = 'Sample subject';
+    $scope.body = 'Sample body,\n\nitems:1\nmodel: Osx#124\n\nRegards,\nDenis Molodtsov';
+
+
+
+
+    $scope.$watch('[to,cc,subject,body]',function(){
+        var mailtto = 'mailto:'+$scope.to +
+            '?cc='+$scope.cc +
+            '&subject='+$scope.to +
+            '&body='+$scope.body;
+             $scope.link = encodeURI(mailtto);
+    }, true);
+});
